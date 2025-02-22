a6f75b3cdf36865e48f26e7821963a4d1af7be44
Vitor Seiji <seijinishida@hotmail.com>
2025-02-22 10:55:13 -0300

    Add files via upload

---
+++ teste 1.cpp
@@ -0,0 +1,16 @@
+//teste 1
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+      int a, b; 
+      float m;
+	  printf("Digite suas notas: ");
+	  scanf("%d %d", &a, &b );
+	  m= (a + b)/2;  
+	    printf("Sua media e: %f\n", m);	
+       
+        system("pause");
+}
+
